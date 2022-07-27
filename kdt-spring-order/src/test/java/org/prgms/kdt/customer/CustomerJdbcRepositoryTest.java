147683287cc614366d92f53e4557fc8d940fbc42
HyoinJeong <hiamy0107@naver.com>
2022-07-27 17:51:03 +0900

    Spring JDBC 2
    test 메소드를 순서에 맞춰 실행
    setup -> insert -> findall -> ...

--- kdt-spring-order/src/test/java/org/prgms/kdt/customer/CustomerJdbcRepositoryTest.java
+++ kdt-spring-order/src/test/java/org/prgms/kdt/customer/CustomerJdbcRepositoryTest.java
@@ -1,8 +1,7 @@
 package org.prgms.kdt.customer;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +22,8 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 @SpringJUnitConfig
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CustomerJdbcRepositoryTest {
     @Configuration
     @ComponentScan(basePackages = {"org.prgms.kdt.customer"})
@@ -48,44 +49,24 @@ class CustomerJdbcRepositoryTest {
     @Autowired
     DataSource dataSource;
 
+    Customer newCustomer;
+
+    @BeforeAll
+    void setup(){
+        newCustomer=new Customer(UUID.randomUUID(),"test-user","test-user@gmail.com", LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS));
+        customerJdbcRepository.deleteAll();
+    }
+
     @Test
+    @Order(1)
     public void testHikariConnectionPool(){
         assertThat(dataSource.getClass().getName(), is("com.zaxxer.hikari.HikariDataSource"));
     }
 
     @Test
-    @DisplayName("전체 고객을 조회할 수 있다.")
-    public void testFindAll() {
-        var customers=customerJdbcRepository.findAll();
-        assertThat(customers.isEmpty(), is(false));
-    }
-
-    @Test
-    @DisplayName("이름으로 고객을 조회할 수 있다.")
-    public void testFindByName() {
-        var customer=customerJdbcRepository.findByName("new-user");
-        assertThat(customer.isEmpty(), is(false));
-
-        var unknown=customerJdbcRepository.findByName("unknown-user");
-        assertThat(unknown.isEmpty(), is(true));
-    }
-
-    @Test
-    @DisplayName("이메일로 고객을 조회할 수 있다.")
-    public void testFindByEmail(){
-        var customer=customerJdbcRepository.findByEmail("new-user@gmail.com");
-        assertThat(customer.isEmpty(), is(false));
-
-        var unknown=customerJdbcRepository.findByEmail("unknown-user@gmail.com");
-        assertThat(unknown.isEmpty(), is(true));
-    }
-
-    @Test
+    @Order(2)
     @DisplayName("고객을 추가할 수 있다.")
     public void testInsert() {
-        customerJdbcRepository.deleteAll();
-
-        var newCustomer=new Customer(UUID.randomUUID(),"test-user","test-user@gmail.com", LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS));
         customerJdbcRepository.insert(newCustomer);
 
         var retrievedCustomer = customerJdbcRepository.findById(newCustomer.getCustomerId());
@@ -93,4 +74,35 @@ class CustomerJdbcRepositoryTest {
         assertThat(retrievedCustomer.get(), samePropertyValuesAs(newCustomer));
 
     }
+
+    @Test
+    @Order(3)
+    @DisplayName("전체 고객을 조회할 수 있다.")
+    public void testFindAll() {
+        var customers=customerJdbcRepository.findAll();
+        assertThat(customers.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("이름으로 고객을 조회할 수 있다.")
+    public void testFindByName() {
+        var customer=customerJdbcRepository.findByName(newCustomer.getName());
+        assertThat(customer.isEmpty(), is(false));
+
+        var unknown=customerJdbcRepository.findByName("unknown-user");
+        assertThat(unknown.isEmpty(), is(true));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("이메일로 고객을 조회할 수 있다.")
+    public void testFindByEmail(){
+        var customer=customerJdbcRepository.findByEmail(newCustomer.getEmail());
+        assertThat(customer.isEmpty(), is(false));
+
+        var unknown=customerJdbcRepository.findByEmail("unknown-user@gmail.com");
+        assertThat(unknown.isEmpty(), is(true));
+    }
+
 }
