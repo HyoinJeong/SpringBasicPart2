bc267d13997066de876b1040b6fda344137d6b7c
HyoinJeong <hiamy0107@naver.com>
2022-07-28 19:02:55 +0900

    JDBC 트랜잭션 처리

--- kdt-spring-order/src/main/java/org/prgms/kdt/JdbcCustomerRepository.java
+++ kdt-spring-order/src/main/java/org/prgms/kdt/JdbcCustomerRepository.java
@@ -1,10 +1,12 @@
 package org.prgms.kdt;
 
+import org.prgms.kdt.customer.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -15,8 +17,8 @@ public class JdbcCustomerRepository {
     private final String SELECT_BY_NAME_SQL = "select * from customers WHERE name=?";
     private final String SELECT_ALL_SQL = "select * from customers";
     private final String INSERT_SQL = "insert into customers(customer_id, name, email) VALUES (UUID_TO_BIN(?), ?, ?)";
-    private final String DELETE_ALL_SQL ="delete from customers";
-    private final String UPDATE_BY_ID_SQL ="update customers set name = ? where customer_id=UUID_TO_BIN(?)";
+    private final String DELETE_ALL_SQL = "delete from customers";
+    private final String UPDATE_BY_ID_SQL = "update customers set name = ? where customer_id=UUID_TO_BIN(?)";
 
 
     public List<String> findNames(String name) {
@@ -72,7 +74,7 @@ public class JdbcCustomerRepository {
         ) {
             while (resultSet.next()) {
                 var customerName = resultSet.getString("name");
-                var customerId =toUUID(resultSet.getBytes("customer_id"));// UUID라서 getBytes
+                var customerId = toUUID(resultSet.getBytes("customer_id"));// UUID라서 getBytes
                 var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
 //                logger.info("customer Id -> {}, name -> {}, createdAt -> {} ", customerId, customerName, createdAt);
                 uuids.add(customerId);
@@ -125,15 +127,55 @@ public class JdbcCustomerRepository {
         return 0;
     }
 
-    static UUID toUUID(byte[] bytes){
-        var byteBuffer=ByteBuffer.wrap(bytes);
+    public void transactionTest(Customer customer) {
+        String updateNameSql = "update customers set name = ? where customer_id=UUID_TO_BIN(?)";
+        String updateEmailSql = "update customers set email = ? where customer_id=UUID_TO_BIN(?)";
+
+        Connection connection=null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "hi01071104");
+            connection.setAutoCommit(false);
+            try (
+                    var updateNameStatement = connection.prepareStatement(updateNameSql);
+                    var updateEmailStatement = connection.prepareStatement(updateEmailSql);
+            ) {
+                updateNameStatement.setString(1, customer.getName());
+                updateNameStatement.setBytes(2, customer.getCustomerId().toString().getBytes());
+                updateNameStatement.executeUpdate();
+
+                updateEmailStatement.setString(1, customer.getEmail());
+                updateEmailStatement.setBytes(2, customer.getCustomerId().toString().getBytes());
+                updateEmailStatement.executeUpdate();
+                connection.setAutoCommit(true);
+
+            }
+        } catch (SQLException e) {
+            if(connection!=null){
+                try {
+                    connection.rollback();
+                    connection.close();
+                } catch (SQLException throwables) {
+                    logger.error("Got error while closing connection", throwables);
+                    throw new RuntimeException(e);
+                }
+            }
+            logger.error("Got error while closing connection", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    static UUID toUUID(byte[] bytes) {
+        var byteBuffer = ByteBuffer.wrap(bytes);
         return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
     }
+
     public static void main(String[] args) {
         var customerRepository = new JdbcCustomerRepository();
 
-        var count=customerRepository.deleteAllCustomers();
-        logger.info("delete count -> {}", count);
+        customerRepository.transactionTest(new Customer(UUID.fromString("43ac12e9-5012-4939-b607-2a7aa05d40d1"), "update-user", "new-user2@gmail.com", LocalDateTime.now()));
+
+//        var count=customerRepository.deleteAllCustomers();
+//        logger.info("delete count -> {}", count);
 
 //        customerRepository.insertCustomer(UUID.randomUUID(),"new-user","new-user@gmail.com");
 //        var customer2 = UUID.randomUUID();
@@ -143,11 +185,11 @@ public class JdbcCustomerRepository {
 //        customerRepository.updateCustomerName(customer2,"updated-user2");
 //        customerRepository.findAllNames().forEach(v -> logger.info("Found name : {}", v));
 
-        var customerId=UUID.randomUUID();
-        logger.info("created customerId -> {}",customerId);
-        logger.info("created UUID Version -> {}",customerId.version());
-
-        customerRepository.insertCustomer(customerId,"new-user","new-user@gmail.com");
-        customerRepository.findAllIds().forEach(v -> logger.info("Found Id : {} and version {}", v, v.version()));
+//        var customerId=UUID.randomUUID();
+//        logger.info("created customerId -> {}",customerId);
+//        logger.info("created UUID Version -> {}",customerId.version());
+//
+//        customerRepository.insertCustomer(customerId,"new-user2","new-user2@gmail.com");
+//        customerRepository.findAllIds().forEach(v -> logger.info("Found Id : {} and version {}", v, v.version()));
     }
 }
